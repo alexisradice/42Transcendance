c47ef8ddff0a498aa54906c119b73522e78b0a1b
wbu <wbu@witbe.team>
2023-12-11 11:44:29 +0100

    rename function in script

--- dev-start.sh
+++ dev-start.sh
@@ -4,16 +4,16 @@ APP_TYPE="$1"
 INSTALL_OPTION="$2"
 
 # Function to run npm install if --install option is provided
-run_npm_install() {
+npm_install() {
   if [ "$INSTALL_OPTION" == "--install" ]; then
     npm install
   fi
 }
 
 if [ "$APP_TYPE" == "front" ]; then
-  cd srcs/frontend/app && run_npm_install && npm run dev
+  cd srcs/frontend/app && npm_install && npm run dev
 elif [ "$APP_TYPE" == "back" ]; then
-  cd srcs/backend/api && run_npm_install && npm run start:dev
+  cd srcs/backend/api && npm_install && npm run start:dev
 else
   echo "Usage: ./dev-start.sh [front|back] [--install]"
 fi
