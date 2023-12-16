fc7cf532d116c3f587052e1a35daad2d09cc1ab1
Woli <wburgos@student.42.fr>
2023-12-16 02:07:26 +0100

    add burgers

--- srcs/frontend/app/src/components/Footer/Footer.tsx
+++ srcs/frontend/app/src/components/Footer/Footer.tsx
@@ -1,9 +1,18 @@
 import { Link } from 'react-router-dom';
 import classes from './Footer.module.css';
+import { Burger } from '@mantine/core';
 
 const Footer = () => (
   <footer className={classes.footer}>
-    <Link to='/terms'>Terms</Link>{' - '}<Link to='/privacy'>Privacy</Link>
+    <div>
+      <Burger opened={false} />
+    </div>
+    <div>
+      <Link to='/terms'>Terms</Link>{' - '}<Link to='/privacy'>Privacy</Link>
+    </div>
+    <div>
+      <Burger opened={false} />
+    </div>
   </footer>
 );
 
--- srcs/frontend/app/src/components/Header/Header.tsx
+++ srcs/frontend/app/src/components/Header/Header.tsx
@@ -1,17 +1,12 @@
 import { Link } from 'react-router-dom';
-import { Burger } from '@mantine/core';
 import ColorSchemeToggle from '../ColorSchemeToggle/ColorSchemeToggle';
 import classes from './Header.module.css';
-import { useState } from 'react';
 
 export default function Header() {
-  const [opened, setOpened] = useState(false);
-  const toggleOpened = () => setOpened((o) => !o);
   return (
     <div className={classes.header}>
       <div className={classes.subsection}>
         <Link to='/'>Pongu!</Link>
-        <Burger opened={opened} onClick={toggleOpened} />
       </div>
       <div className={classes.subsection}>
         {/* <a href="http://localhost:3000/auth" target="_blank" rel="noopener noreferrer">Login</a> */}
