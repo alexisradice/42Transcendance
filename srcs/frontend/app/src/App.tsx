a8ffe9c51079f13c81500e93a3427fd84e7a2291
wbu <wbu@witbe.team>
2023-12-17 22:15:28 +0100

    add oauth2 loop

--- srcs/frontend/app/src/App.tsx
+++ srcs/frontend/app/src/App.tsx
@@ -13,10 +13,29 @@ const authConfig: TAuthConfig = {
 	authorizationEndpoint: "https://api.intra.42.fr/oauth/authorize",
 	tokenEndpoint: "https://api.intra.42.fr/oauth/token",
 	redirectUri: import.meta.env.VITE_REDIRECT_URI,
-	scope: "public",
 	autoLogin: false,
+	decodeToken: false,
 	extraTokenParameters: {
 		client_secret: import.meta.env.VITE_CLIENT_SECRET,
+	},
+	scope: "public",
+	postLogin: () => {
+		fetch(`${import.meta.env.VITE_API_URL}/auth`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ token: localStorage.getItem("ROCP_token") }),
+		})
+			.then((response: Response) => {
+				return response.json();
+			})
+			.then((json: { token: string }) => {
+				localStorage.setItem("JWT_token", json.token);
+			})
+			.catch((err: Error) => {
+				console.error(err);
+			});
 	},
 	onRefreshTokenExpire: (event: TRefreshTokenExpiredEvent) =>
 		window.confirm(
