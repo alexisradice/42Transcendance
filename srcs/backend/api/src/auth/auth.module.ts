107b07d05a7e4c50444c4a978f9a70ecc9ff3813
lea <bitbucket>
2023-12-17 22:12:49 +0100

    generate jwt token ok

--- srcs/backend/api/src/auth/auth.module.ts
+++ srcs/backend/api/src/auth/auth.module.ts
@@ -2,12 +2,27 @@ import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { AuthStrategy } from "./auth.strategy";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { UserService } from "src/user/user.service";
+import { JwtModule, JwtService } from "@nestjs/jwt";
+import { UserModule } from "src/user/user.module";
 
 @Module({
-	imports: [ConfigModule],
+	imports: [
+		ConfigModule,
+		UserModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			useFactory: async (configService: ConfigService) => ({
+				global: true,
+				secret: configService.get<string>("JWT_SECRET"),
+				signOptions: { expiresIn: "60s" },
+			}),
+			inject: [ConfigService],
+		}),
+	],
 	controllers: [AuthController],
-	providers: [UserService, AuthStrategy, AuthService],
+	providers: [UserService, JwtService, AuthService],
+	exports: [AuthService],
 })
 export class AuthModule {}
--- srcs/backend/api/src/auth/auth.service.ts
+++ srcs/backend/api/src/auth/auth.service.ts
@@ -1,12 +1,53 @@
-import { Injectable } from "@nestjs/common";
-import { Req } from "@nestjs/common";
-import { Request } from "express";
+import { HttpException, Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { JwtService } from "@nestjs/jwt";
+import { MiniUser } from "src/types";
+import { UserService } from "src/user/user.service";
 
 @Injectable()
 export class AuthService {
-	constructor() {}
+	constructor(
+		private userService: UserService,
+		private jwtService: JwtService,
+		private configService: ConfigService,
+	) {}
 
-	async signIn(@Req() req: Request) {
-		return req.user;
+	async validate(accessToken): Promise<MiniUser> {
+		const url = "https://api.intra.42.fr/v2/me";
+		let status;
+		try {
+			const response = await fetch(url, {
+				method: "GET",
+				headers: {
+					Authorization: `Bearer ${accessToken}`,
+				},
+			});
+			status = response.status;
+			if (!response.ok) {
+				throw new TypeError(response.statusText);
+			}
+			const userProfile = await response.json();
+			return {
+				email: userProfile.email,
+				login: userProfile.login,
+				image: userProfile.image.link,
+			};
+		} catch (error) {
+			throw new HttpException(error.message, status || 500);
+		}
+	}
+
+	async fromOauthToJwt(oauthToken: string) {
+		const userInfo = await this.validate(oauthToken);
+		const payload = {
+			sub: userInfo.login,
+			email: userInfo.email,
+			image: userInfo.image,
+		};
+		await this.userService.findOrCreate(userInfo);
+		const option = { secret: this.configService.get<string>("JWT_SECRET") };
+		return {
+			jwtToken: await this.jwtService.signAsync(payload, option),
+		};
 	}
 }
