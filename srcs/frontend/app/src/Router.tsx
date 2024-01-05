12d90bc2cc4a14b92bacb584a0e3d1b5f6d8ee3e
wbu <willy.burgos@witbe.net>
2024-01-05 01:01:45 +0100

    remove helloPage from router

--- srcs/frontend/app/src/Router.tsx
+++ srcs/frontend/app/src/Router.tsx
@@ -1,6 +1,5 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import ErrorPage from "./pages/Error.page";
-import { HelloPage } from "./pages/Hello.page";
 import { GamePage } from "./pages/Game.page";
 import { MainPage } from "./pages/Main.page";
 
@@ -11,8 +10,8 @@ const router = createBrowserRouter([
 		errorElement: <ErrorPage />,
 		children: [
 			{
-				path: "hello",
-				element: <HelloPage />,
+				path: "",
+				element: <></>,
 			},
 			{
 				path: "game",
